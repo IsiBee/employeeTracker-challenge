37112aed5f3a2e41ebae46225ac0997a8bdfdcd5
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-03-07 09:44:31 -0600

    added employee table schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,8 @@ USE employee_tracker_db;
 
 
 CREATE TABLE departments(
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles(
@@ -15,4 +14,14 @@ CREATE TABLE roles(
     salary DECIMAL UNSIGNED,
     department_id INTEGER UNSIGNED,
     CONSTRAINT fk_dept_id FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employees(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER UNSIGNED,
+    manager_id INTEGER UNSIGNED,
+    CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager_id FOREIGN KEY (id) REFERENCES employees(id) ON DELETE SET NULL
 );
